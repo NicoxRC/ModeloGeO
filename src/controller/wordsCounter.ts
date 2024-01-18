45acc590fce97104483194b6452b55aeda178d23
NicoxRC <nicorojas005@gmail.com>
2024-01-18 08:06:02 -0500

    changing words counter

--- src/controller/wordsCounter.ts
+++ src/controller/wordsCounter.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from 'express';
+import type { WordsCounterInterface } from '../interfaces/wordsCounter';
 
 export const getWordsCounter = async (
   req: Request,
@@ -7,21 +8,28 @@ export const getWordsCounter = async (
   const { text } = req.body;
 
   try {
-    function contarPalabras(text: string) {
+    function contarPalabras(text: string): WordsCounterInterface[] {
       const palabras: string[] = text
         .toLowerCase()
         .replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '')
         .split(' ');
-      const conteoPalabras: any = {};
-      palabras.forEach((palabra: string) => {
-        if (conteoPalabras[palabra]) {
-          conteoPalabras[palabra]++;
-        } else {
-          conteoPalabras[palabra] = 1;
+
+      const conteoPalabras: WordsCounterInterface[] = [];
+      palabras.forEach((palabra) => {
+        if (palabra.length > 3) {
+          const indice = conteoPalabras.findIndex(
+            (item) => item.palabra === palabra
+          );
+          if (indice !== -1) {
+            conteoPalabras[indice].cantidad++;
+          } else {
+            conteoPalabras.push({ palabra, cantidad: 1 });
+          }
         }
       });
       return conteoPalabras;
     }
+
     const resultado = contarPalabras(text);
     res.status(200).json(resultado);
   } catch (error: unknown) {
